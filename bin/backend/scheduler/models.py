93c65d853eb8d7af680a3311a3d8931c97a4417e
mmcdaniel44 <mmcdaniel44@gatech.edu>
2021-02-05 16:55:33 -0500

    editing models to format criteria in db  and running migrations

--- bin/backend/scheduler/models.py
+++ bin/backend/scheduler/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+# add this
+class Criteria(models.Model):
+    RotationType = models.CharField(max_length=120)
+    TypeAmount = models.PositiveIntegerField()
+
+    def _str_(self):
+        return "RotationType: " + self.RotationType + " TypeAmount: " + self.TypeAmount
+
+    def get_RotationType(self):
+        return self.RotationType
+
+    def get_TypeAmount(self):
+        return self.TypeAmount
+
+    def set_RotationType(self, RotationType):
+        self.RotationType = RotationType
+
+    def set_TypeAmount(self, TypeAmount):
+        self.TypeAmount = TypeAmount
