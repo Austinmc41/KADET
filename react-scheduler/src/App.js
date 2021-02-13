b7a36084d480c71f16d65229fabbb37dff0b9b99
Terrence Shi <terrenceshi@gmail.com>
2021-02-13 15:36:15 -0500

    Update App.js

--- react-scheduler/src/App.js
+++ react-scheduler/src/App.js
@@ -6,6 +6,7 @@ class App extends React.Component {
     render(){
         return (
             <div className="App">
+            
                 <form name="formFiltre" id="formFiltre" method="post">
                     <div class="form-inline">
                         <div class="form-group ">
