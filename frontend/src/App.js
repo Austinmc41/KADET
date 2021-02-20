7fb8e19fe1b65fd736a5bfa7682e06274ba718cd
Eduardo <eduardo.rodriguez@gatech.edu>
2021-02-20 17:23:23 -0500

    added working, non-functional page to frontend. It is not yet connected to backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,128 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+const todoItems = [
+  {
+    id: 1,
+    title: "ipsum dolor",
+    description: "consectetur adipiscing elit",
+    completed: true,
+  },
+  {
+    id: 2,
+    title: "sit amet",
+    description: "sed do eiusmod tempor incididunt",
+    completed: false,
+  },
+  {
+    id: 3,
+    title: "enim ad",
+    description: "quis nostrud exercitation ullamco",
+    completed: true,
+  },
+  {
+    id: 4,
+    title: "minim veniam",
+    description: "laboris nisi ut aliquip ex ea commodo",
+    completed: false,
+  },
+];
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      viewCompleted: false,
+      todoList: todoItems,
+    };
+  }
+
+  displayCompleted = (status) => {
+    if (status) {
+      return this.setState({ viewCompleted: true });
+    }
+
+    return this.setState({ viewCompleted: false });
+  };
+
+  renderTabList = () => {
+    return (
+      <div className="nav nav-tabs">
+        <span
+          className={this.state.viewCompleted ? "nav-link active" : "nav-link"}
+          onClick={() => this.displayCompleted(true)}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          Complete
+        </span>
+        <span
+          className={this.state.viewCompleted ? "nav-link" : "nav-link active"}
+          onClick={() => this.displayCompleted(false)}
+        >
+          Incomplete
+        </span>
+      </div>
+    );
+  };
+
+  renderItems = () => {
+    const { viewCompleted } = this.state;
+    const newItems = this.state.todoList.filter(
+      (item) => item.completed == viewCompleted
+    );
+
+    return newItems.map((item) => (
+      <li
+        key={item.id}
+        className="list-group-item d-flex justify-content-between align-items-center"
+      >
+        <span
+          className={`todo-title mr-2 ${
+            this.state.viewCompleted ? "completed-todo" : ""
+          }`}
+          title={item.description}
+        >
+          {item.title}
+        </span>
+        <span>
+          <button
+            className="btn btn-secondary mr-2"
+          >
+            Edit
+          </button>
+          <button
+            className="btn btn-danger"
+          >
+            Delete
+          </button>
+        </span>
+      </li>
+    ));
+  };
+
+  render() {
+    return (
+      <main className="container">
+        <h1 className="text-uppercase text-center my-4">Resident Scheduler app</h1>
+        <p><h4 className="text-uppercase text-center my-4">(non-functional, just display)</h4></p>
+        <div className="row">
+          <div className="col-md-6 col-sm-10 mx-auto p-0">
+            <div className="card p-3">
+              <div className="mb-4">
+                <button
+                  className="btn btn-primary"
+                >
+                  Add criteria
+                </button>
+              </div>
+
+              <ul className="list-group list-group-flush border-top-0">
+                {this.renderItems()}
+              </ul>
+            </div>
+          </div>
+        </div>
+      </main>
+    );
+  }
 }
 
 export default App;
