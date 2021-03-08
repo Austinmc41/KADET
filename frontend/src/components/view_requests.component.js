651f6b442b8425109340e8cedd00b9e7cb18d41c
Terrence Shi <terrenceshi@gmail.com>
2021-03-08 13:41:03 -0500

    basic functionality for request page

--- frontend/src/components/view_requests.component.js
+++ frontend/src/components/view_requests.component.js
@@ -27,7 +27,7 @@ class Requests extends Component {
     refreshList = () => {
         axios
           .get("/requests/api/")
-          .then((res) => this.setState({ criteriaList: res.data }))
+          .then((res) => this.setState({ requestList: res.data }))
           .catch((err) => console.log(err));
     };
 
@@ -35,58 +35,55 @@ class Requests extends Component {
         const newItems = this.state.requestList;
 
         return newItems.map((item) => (
-          <li
-            key={item.id}
-            className="list-group-item d-flex justify-content-between align-items-center"
-          >
-            <span
-              className={`mr-2`}
-            >
-              {item.RotationType}
-            </span>
+            
+            <Card>
+                <div className="col-md-8 col-sm-10 mx-auto p-0">
 
-          </li>
+                    <Accordion.Toggle as={Card.Header} eventKey="0">
+                    '"First Name" + "Lastname"''
+                    </Accordion.Toggle>
+                    <Accordion.Collapse eventKey="0">
+                    <Card.Body>
+                    <p>Email Address:</p> {item.email}
+                    <p>Request 1:</p>
+                    <p>Request 2:</p>
+                    <p>Request 3:</p>
+                    </Card.Body>
+                    </Accordion.Collapse>
+
+                </div>
+            
+            </Card>
+
         ));
     };
 
     render() {
     return (
         <main className="container">
-        <h3 className="text-center">Schedule Requests</h3>
-        <div className="row">
-          <div className="col-md-8 col-sm-10 mx-auto p-0">
-            <div className="card p-3">
-        <Accordion defaultActiveKey="">
+            <h3 className="text-center">Schedule Requests</h3>
+            <div className="row">
+                <div className="col-md-8 col-sm-10 mx-auto p-0">
+                    <div className="card p-3">
+                <Accordion defaultActiveKey="">
 
-        <Card>
-        
-        <Accordion.Toggle as={Card.Header} eventKey="0">
-        '"First Name" + "Lastname"''
-        </Accordion.Toggle>
-        <Accordion.Collapse eventKey="0">
-        <Card.Body>
-        <p>Email Address:</p>
-        <p>Request 1:</p>
-        <p>Request 2:</p>
-        <p>Request 3:</p>
-        </Card.Body>
-        </Accordion.Collapse>
-        
-        </Card>
+                <ul className="list-group list-group-flush border-top-0">
+                    {this.renderRequests()}
+                </ul>
 
-        
-        <Card>
-        <Accordion.Toggle as={Card.Header} eventKey="1">
-        Click me!
-        </Accordion.Toggle>
-        <Accordion.Collapse eventKey="1">
-        <Card.Body>Hello! I'm another body</Card.Body>
-        </Accordion.Collapse>
-        </Card>
-        </Accordion>
-        </div>
-        </div>
-        </div>
+
+                <Card>
+                <Accordion.Toggle as={Card.Header} eventKey="1">
+                Click me!
+                </Accordion.Toggle>
+                <Accordion.Collapse eventKey="1">
+                <Card.Body>Hello! I'm another body</Card.Body>
+                </Accordion.Collapse>
+                </Card>
+                </Accordion>
+                </div>
+                </div>
+            </div>
         </main>
         );
     };
