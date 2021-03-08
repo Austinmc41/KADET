0a1a39b46b1d54bfc5ebe0379dd3fc0b9164bf46
Terrence Shi <terrenceshi@gmail.com>
2021-03-08 13:14:13 -0500

    minor changes

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -4,7 +4,7 @@ import { Router, Switch, Route } from "react-router-dom";
 import Home from "./components/Home";
 import Settings from "./components/settings.component";
 import Criteria from "./components/criterion.component";
-import ListRequests from "./components/view_requests";
+import ListRequests from "./components/view_requests.component";
 import Two from "./components/two";
 import Three from "./components/three";
 import NotFound from "./components/NotFound";
