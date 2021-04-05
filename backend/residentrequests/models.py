eea0145beb6e74f2cc30a7a7cd9e93305007826e
Eduardo <eduardo.rodriguez@gatech.edu>
2021-04-04 21:43:10 -0400

    updated ResidentRequests to have a one-to-one relationship with SchedulerUser

--- backend/residentrequests/models.py
+++ backend/residentrequests/models.py
@@ -1,7 +1,12 @@
 from django.db import models
+from useraccess.models import SchedulerUser
 
 class ResidentRequests(models.Model):
-    email = models.EmailField()
+    email = models.OneToOneField(
+        SchedulerUser,
+        on_delete=models.CASCADE,
+        primary_key=True,
+    )
     requestOne = models.DateField()
     requestTwo = models.DateField()
     requestThree = models.DateField()
