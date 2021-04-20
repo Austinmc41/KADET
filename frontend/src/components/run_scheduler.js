246818b1132ff42893535b3126c9371c8d25f2ae
Eduardo <eduardo.rodriguez@gatech.edu>
2021-04-19 22:07:07 -0400

    Updated run_scheduler to access backend

--- frontend/src/components/run_scheduler.js
+++ frontend/src/components/run_scheduler.js
@@ -1,18 +1,27 @@
-import React, { Component } from "react";
-import { Button } from 'react-bootstrap';
-import history from '../history';
+import React, { Component } from "react"; 
+import { Button } from 'react-bootstrap'; 
+import history from '../history'; 
+import axios from "axios"; 
+ 
+export default class run_scheduler extends Component { 
+    render() { 
+        return ( 
+            <div className="container"> 
+                <div className="text-center"> 
+                    <h3>Let the algorithm do its magic!</h3> 
+                    <form onSubmit={this.handleSubmit}> 
+                    <input className="btn btn-primary" type="submit" value="Run Scheduler !"/> 
+                    </form> 
+                </div> 
+            </div> 
+        ); 
+    } 
 
-export default class run_scheduler extends Component {
-    render() {
-        return (
-            <div className="container">
-                <div className="text-center">
-                    <h3>Let the algorithm do its magic!</h3>
-                    <form>
-                        <Button variant="btn btn-success" onClick={() => history.push('/Settings')}>Generate Schedule</Button>
-                    </form>
-                </div>
-            </div>
-        );
-    }
-}
+handleSubmit = (item) => { 
+    axios 
+        .post("", item) 
+        .then((res) => this.refreshList()); 
+    }; 
+} 
+
+ 
