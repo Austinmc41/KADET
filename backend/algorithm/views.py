3cc6a6e944904d9333ac3e41626900a1701754a5
Eduardo <eduardo.rodriguez@gatech.edu>
2021-04-25 18:09:04 -0400

    Filling in code to insert dats as week dictionaries in JSON field of SchedulerUser model

--- backend/algorithm/views.py
+++ backend/algorithm/views.py
@@ -5,8 +5,17 @@ from rest_framework import viewsets
 from .serializers import StatusSerializer
 from .models import AlgorithmStatus
 
+from criteria.models import Criteria
+from useraccess.models import SchedulerUser
+from residentrequests.models import ResidentRequests
+
 class StatusView(viewsets.ModelViewSet):
     serializer_class = StatusSerializer
     # queryset = AlgorithmStatus.objects.all()
     def get_queryset(self):
+        for resident in SchedulerUser.objects.all():
+            if resident.AccessLevel != 'not applicable':
+                
         return AlgorithmStatus.objects.all()
+
+        # AlgorithmStatus.objects.create
