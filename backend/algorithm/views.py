5468aa50e856c9fa84ea9099e9a920a338686b86
Eduardo <eduardo.rodriguez@gatech.edu>
2021-04-25 20:03:17 -0400

    Added getWeekDelta function for algorithm

--- backend/algorithm/views.py
+++ backend/algorithm/views.py
@@ -5,17 +5,37 @@ from rest_framework import viewsets
 from .serializers import StatusSerializer
 from .models import AlgorithmStatus
 
+import math
+from datetime import date
 from criteria.models import Criteria
 from useraccess.models import SchedulerUser
 from residentrequests.models import ResidentRequests
+from settings.models import Settings
+
+def getWeekDelta(startDate, endDate):
+    #assume that every criterion and the schedule itself starts on Wednesday, per Chris
+
+    scheduleStart = Settings.StartSchedule
+    
+    #the idea is that both of these variables should be DateTimeField objects and we should be able to get the difference in days
+
+    difference = endDate.date() - startDate.date()
+    delta = difference.days
+
+    weeks = math.ceil(delta / 7) #we use ceiling to round up all days ex: if delta = 10 that should be considered 2 weeks
+    return weeks
 
 class StatusView(viewsets.ModelViewSet):
     serializer_class = StatusSerializer
     # queryset = AlgorithmStatus.objects.all()
     def get_queryset(self):
+        scheduleStart = Settings.StartSchedule
         for resident in SchedulerUser.objects.all():
             if resident.AccessLevel != 'not applicable':
-                
+                requests = ResidentRequests.objects.get(pk=resident.email)
+                requestOne = requests.requestOne
+                requestTwo = requests.requestTwo
+                requestThree = requests.requestThree
         return AlgorithmStatus.objects.all()
 
         # AlgorithmStatus.objects.create
