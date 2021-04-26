fda393fd57736d6ce603703c6b3bcc836db412f6
Eduardo <eduardo.rodriguez@gatech.edu>
2021-04-25 23:49:43 -0400

    Adding black out dates now functional

--- backend/algorithm/views.py
+++ backend/algorithm/views.py
@@ -16,7 +16,7 @@ from settings.models import Settings
 def getWeekDelta(startDate, endDate):
     #assume that every rotation and the schedule itself starts on Wednesday, per Chris
     #the idea is that both of these variables should be DateTimeField objects and we should be able to get the difference in days
-    difference = datetime.datetime(endDate) - datetime.datetime(startDate)
+    difference = endDate - startDate
     delta = difference.days
 
     weeks = math.ceil(delta / 7) #we use ceiling to round up all days ex: if delta = 10 that should be considered 2 weeks
@@ -26,29 +26,23 @@ class StatusView(viewsets.ModelViewSet):
     serializer_class = StatusSerializer
     # queryset = AlgorithmStatus.objects.all()
     def get_queryset(self):
-        scheduleStart = Settings.StartSchedule
+        scheduleStart = Settings.objects.get(pk=1).StartSchedule
         messageOne = AlgorithmStatus(Status='Adding resident requests to schedule')
         messageOne.save()
         for resident in SchedulerUser.objects.all():
             if resident.AccessLevel != 'NA':
-                loopMessage = AlgorithmStatus(Status='looking up user ' + str(resident.email) + ' with access level ' + str(resident.AccessLevel))
-                loopMessage.save()
                 requests = ResidentRequests.objects.get(pk=resident.email)
                 requestOne = requests.requestOne
                 requestTwo = requests.requestTwo
                 requestThree = requests.requestThree
-
-                loopMessage2 = AlgorithmStatus(Status='{ ' + str(requestOne) + ' }, ' + 'type: ' + str(type(requestOne)))
-                loopMessage2.save()
-                loopMessage3 = AlgorithmStatus(Status='{ ' + str(scheduleStart) + ' }, ' + 'type: ' + str(type(scheduleStart)))
-                loopMessage3.save()
-                loopMessage4 = AlgorithmStatus(Status='{ ' + str(requests.email) + ' }, ' + 'type: ' + str(type(requests.email)))
-                loopMessage4.save()
-
                 weekOfRequestOne = str(getWeekDelta(scheduleStart, requestOne))
                 weekOfRequestTwo = str(getWeekDelta(scheduleStart, requestTwo))
                 weekOfRequestThree = str(getWeekDelta(scheduleStart, requestThree))
                 resident.ResidentSchedule.update({weekOfRequestOne: "VACATION"})
+                resident.save()
+                resident.ResidentSchedule.update({weekOfRequestTwo: "VACATION"})
+                resident.save()
+                resident.ResidentSchedule.update({weekOfRequestThree: "VACATION"})
                 resident.save()
         messageTwo = AlgorithmStatus(Status='Black out dates now added')
         messageTwo.save()
